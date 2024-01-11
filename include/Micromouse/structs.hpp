1fffb33e79867baf76b27c30b67411f7020b2c85
Yujiro Shito <s.yujiro613@gmal.com>
2024-01-11 18:08:32 +0900

    [Update] 構造体をまとめた

--- include/Micromouse/structs.hpp
+++ include/Micromouse/structs.hpp
@@ -268,5 +268,239 @@ typedef enum {
 } i2c_port_t;
 
 
+/* 
+大きく４つの構造体グループに分けて参照渡しする。
+1. センサー関連
+2. マウス動作関連
+3. 制御関連
+4. マップ関連
+*/
+
+typedef enum
+{
+    FALSE = 0,
+    TRUE = 1,
+}t_bool;
+
+typedef enum
+{
+    FRONT = 0,
+    RIGHT = 1,
+    REAR = 2,
+    LEFT = 3,
+    UNDEFINED,
+}t_local_dir;
+
+typedef enum
+{
+    NORTH = 0,
+    EAST = 1,
+    SOUTH = 2,
+    WEST = 3,
+}t_direction;
+
+typedef enum
+{
+    NOWALL = 0,
+    WALL = 1,
+    UNKNOWN = 2,
+}t_exist_wall;
+
+typedef struct 
+{
+    int f = 0;  //front
+    int fl = 0; //front left
+    int fr = 0; //front right
+    int l = 0;  //left
+    int r = 0;  //right
+    int b = 0;  //back
+}t_sens_dir;    //sensor direction data
+
+typedef struct 
+{
+    t_bool l = FALSE; //front
+    t_bool fl = FALSE;    //front left
+    t_bool fr = FALSE;    //front right
+    t_bool r = FALSE; //left
+}t_wall_exist;  //wall exist data
+
+typedef struct 
+{
+    t_sens_dir val;  //sensor value
+    t_sens_dir d_val;    //sensor value difference
+    t_sens_dir p_val;    //sensor value past
+    t_sens_dir error;    //sensor value error
+    t_sens_dir ref;  //sensor value reference
+    t_sens_dir th_wall;  //wall threshold value
+    t_sens_dir th_control;   //control threshold value
+    t_wall_exist exist_wall; //wall true or false
+    t_wall_exist control_enable;  //control true or false
+    t_bool wall_control;  //enable or disable
+}t_wall_sens;  //wall sensor data
+
+typedef struct 
+{
+    float yaw = 0; //gyro yaw
+    float yaw_new = 0; //gyro yaw new
+    float ref = 0; //gyro reference
+    float degree = 0;
+    float radian = 0;
+}t_gyro;    //gyro data
+
+typedef struct 
+{
+    unsigned int angle = 0;
+    t_sens_dir data;
+    t_sens_dir locate;
+    t_sens_dir p_locate;
+    t_sens_dir diff_pulse;
+    t_sens_dir diff_p_pulse;    
+}t_enc;     //encoder data
+
+typedef struct 
+{
+    t_wall_sens wall;
+    t_gyro gyro;
+    t_enc enc;
+}t_sens_data;   //sensor data
+
+typedef struct
+{
+    float accelX = 0;
+    float accelY = 0;
+    float accelZ = 0;
+    float gyroX = 0;
+    float gyroY = 0;
+    float gyroZ = 0;
+    uint16_t Angle = 0;
+    float BatteryVoltage = 0;
+    int sens_r_value = 0;
+    int sens_l_value = 0;
+    int sens_fr_value = 0;
+    int sens_fl_value = 0;
+}t_sensing_result;
+
+
+
+typedef struct 
+{
+    float vel = 0;  //velocity
+    float ang_vel = 0;  //angular velocity
+    float deg = 0;  //degree
+    float rad = 0;  //radian
+    float vel_error = 0; //error
+    float ang_error = 0;    //angular error
+    float acc = 0;  //acceleration
+    float ang_acc = 0;  //angular acceleration
+    float len = 0;   //length
+    float wall_val = 0; //wall value
+    float wall_error = 0;   //wall error
+    float alpha = 0;    //相補フィルタ用
+    t_local_dir flag;
+}t_motion;  //motion parameter
+
+
+
+typedef struct 
+{
+    t_motion r;
+    t_motion l;
+    t_motion p;    //past
+    t_motion current;   //current
+    t_motion max;  //max
+    t_motion min;  //min
+    t_motion end;   //end
+    t_motion tar;   //target
+    t_motion sum;   //sum
+    t_motion I;    //integral
+}t_mouse_motion_val;    //motion value
+
+typedef struct 
+{
+    float tire_diameter = 0;
+    float tire_radius = 0;
+    //float R;
+    float Kt = 0;
+    float Ke = 0;
+    float truque = 0;
+}t_motor;   //motor parameter
+
+typedef struct 
+{
+    float Kp = 0;   //proportional gain
+    float Ki = 0;   //integral gain
+    float Kd = 0;   //differential gain
+}t_pid; //pid parameter
+
+typedef struct 
+{
+    float x_pos = 0;
+    float y_pos = 0;
+}t_odom;    //odometry data
+
+typedef struct 
+{
+    t_pid v;    //velocity pid
+    t_pid o;    //omega pid
+    t_pid d;    //degree pid
+    t_pid wall; //wall pid
+    float Vatt = 0;
+    float V_l = 0;
+    float V_r = 0;
+    float Duty_l = 0;
+    float Duty_r = 0;
+    int time_count = 0;
+    t_bool control_flag = FALSE;
+    t_motor mot;
+    t_odom odom;
+}t_control; //control parameter
+
+
+typedef struct 
+{
+    unsigned char north:2;
+    unsigned char east:2;
+    unsigned char south:2;
+    unsigned char west:2;
+    t_bool flag;
+}t_wall;    //wall data
+
+typedef struct
+{
+    short x = 0;
+    short y = 0;
+    t_direction dir;
+}t_pos;     //position data
+
+typedef struct 
+{
+    t_pos pos;
+    t_wall wall[32][32];
+    unsigned char size[32][32];
+}t_map;     //map data
+
+
+
+
+
+
+
+//extern t_bool flag;
+//extern t_local_dir l_dir;
+//extern t_sens_dir s_dir;
+//extern t_wall_sens sens.wall;
+//extern t_gyro gyro;
+//extern t_enc enc;
+extern t_sens_data sens;
+//extern t_motion motion;
+extern t_mouse_motion_val m_val;
+//extern t_motor mot;
+//extern t_pid pid;
+extern t_control ctl;
+//extern t_wall wall;
+extern t_map map;
+//extern t_pos mypos;
+//extern t_odom odom;
+
 
 #endif // STRUCTS_HPP
